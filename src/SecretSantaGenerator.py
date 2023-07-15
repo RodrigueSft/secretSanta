dbeb84d641ea31f51bafab7beb3e2c08ae760a3b
Rodrigue Saillenfest <rodrigue.saillenfest@360learning.com>
2023-07-15 15:49:12 +0200

    feat: first version working - need to add automated test for it   - Coming in the next commit - also not perfect, it can "deadlock" to it

--- src/SecretSantaGenerator.py
+++ src/SecretSantaGenerator.py
@@ -1,3 +1,6 @@
+import random
+
+
 class SecretSantaGenerator:
     people = []
     couples = []
@@ -9,3 +12,24 @@ class SecretSantaGenerator:
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}:\n people={self.people},\n couple={self.couples},\n pairs={self.pairs}\n"
+
+    def generate(self):
+        # prep work
+        people_set = set(self.people)
+        s = dict()
+        gifted = set()
+        for person in self.people:
+            s[person] = {person}
+        for a, b in self.couples:
+            s[a].add(b)
+            s[b].add(a)
+
+        for p in self.people:
+            available = people_set ^ (s[p] | gifted)
+            # reset if receivers is empty because dead end
+            receivers = random.sample(available, 1)
+
+            self.pairs.append((p, receivers[0]))
+            gifted.add(receivers[0])
+            s[receivers[0]].add(p)
+
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def main():
     people = ["Florent", "Jessica", "Coline", "Emilien", "Ambroise", "Bastien"]
     couples = [("Florent", "Jessica"), ("Coline", "Emilien")]
     secret_santa = SecretSantaGenerator(people, couples)
+    secret_santa.generate()
     print(secret_santa)
 
 
